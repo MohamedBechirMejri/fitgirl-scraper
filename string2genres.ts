a4434971762efa1ea37ff1ebb7ebf7720ab2b08f
MohamedBechirMejri <mohamed.bechir.mejri.dev@gmail.com>
2024-07-25 15:58:47 +0100

    add genres section

---
+++ string2genres.ts
@@ -0,0 +1,36 @@
+import * as fs from "fs/promises";
+
+function main() {
+  fs.readFile("scraped_data.json", "utf-8")
+    .then(data => {
+      const posts = JSON.parse(data);
+
+      const keys = Object.keys(posts);
+
+      for (const key of keys) {
+        const post = posts[key];
+
+        const { "Genres/Tags": genres } = post.info;
+
+        if (!genres) {
+          continue;
+        }
+
+        const genresArray = genres.split(", ");
+
+        console.log(genres);
+        console.log(genresArray);
+
+        post.info.genres = genresArray;
+
+        posts[key] = post;
+      }
+      // save file
+      fs.writeFile("scraped_data.json", JSON.stringify(posts, null, 2));
+    })
+    .catch(error => {
+      console.error(error);
+    });
+}
+
+main();
